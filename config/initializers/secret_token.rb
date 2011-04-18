c1a0157c9879fd5edfceb50be748a837218ceea8
U-wbrands2-PC\wbrands2 <wbrands2@wbrands2-PC.(none)>
2011-04-17 21:03:33 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '0433ae74c9012d6e27d816a7d54b00178b1cda0f5aaaf46050d66416b985695a4218bc71d2413c08a7a18487db31a5237cf735b922dabbde135f44fabe5b2153'
